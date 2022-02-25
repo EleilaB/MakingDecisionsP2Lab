a0d2428e48d6ff52057b48afaef800233c783cb1
Eleila Bradford <eleila.bradford@gmail.com>
2022-02-24 21:49:37 -0700

    Problem 11 solved

--- MD2Lab.js
+++ MD2Lab.js
@@ -208,3 +208,15 @@ switch(letterGrade){
 */
 
 //Code Here
+
+for(let i = 1; i <= 100; i++){
+  if(i % 3 === 0 && i % 5 === 0){
+    console.log(`DevMountain`)
+  }else if(i % 3 === 0){
+    console.log(`Dev`)
+  }else if(i % 5 === 0){
+    console.log(`Mountain`)
+  }else{
+    console.log(i)
+  }
+}
